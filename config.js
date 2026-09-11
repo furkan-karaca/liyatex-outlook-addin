0b4193fe1e77c41516b5f8fc261cb2061060d265
Furkan Karaca <furkankaracagalaxy@gmail.com>
2026-09-11 16:33:45 +0300

    client id

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // CLIENT_ID: liyatexcrm kiracısında açılacak "Liyatex Outlook Eklentisi" uygulama kaydının Application (client) ID'si.
 // HOST_URL : eklentinin yayımlandığı HTTPS kök adres (manifest.xml içindeki adresle birebir aynı olmalı).
 window.LIY_CFG = {
-  CLIENT_ID: "00000000-0000-0000-0000-000000000000",
+  CLIENT_ID: "812ac390-2736-404c-9494-3463cc2b70d2",
   TENANT_ID: "e162e6fc-1efd-4892-9039-626a81816b87",
   CRM_URL: "https://liyatexprod.crm4.dynamics.com",
   HOST_URL: "https://furkan-karaca.github.io/liyatex-outlook-addin",
